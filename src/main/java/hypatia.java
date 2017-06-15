e136e227f581f92f09be633dfc5ee2d3780be68d
co17 <clifforeilly@gmail.com>
2017-06-15 16:36:51 +0100

    Initial commit

---
+++ src/main/java/hypatia.java
@@ -0,0 +1,15 @@
+/**
+ * Created by co17 on 15/06/2017.
+ */
+public class hypatia {
+
+    public static void main (String[] args){
+
+        System.out.println("starting...");
+
+
+
+        System.out.println("ending...");
+    }
+    
+}
